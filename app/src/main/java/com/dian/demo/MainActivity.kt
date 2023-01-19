40674fe7b26676ad6db42c3b2a4233fec8ce284f
fanqingdian <fanqingdian@data-baker.com>
2023-01-19 12:58:59 +0800

    Initial commit

---
+++ app/src/main/java/com/dian/demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dian.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * appkey:7bf0f3de89c9206aab25f1b2ff7da4cd
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
